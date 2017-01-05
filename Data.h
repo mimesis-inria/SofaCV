3eab2c619d77af544cf7f253ba12ca7d2c80e146
Bruno Marques <bruno.josue.marques@inria.fr>
2017-01-05 11:48:26 +0100

    - Adding readme - Implemented IGTLink client / server components (only receiving features for now) - Implemented IGTLinkBaseSocket, BaseGrabberIGT and BaseFrameGrabber

---
+++ Data.h
@@ -0,0 +1,18 @@
+#ifndef SOFAOR_COMMON_DATA_H
+#define SOFAOR_COMMON_DATA_H
+
+namespace sofa
+{
+namespace OR
+{
+namespace common
+{
+struct Data
+{
+};
+
+}  // namespace common
+}  // namespace OR
+}  // nammespace sofa
+
+#endif  // SOFAOR_COMMON_DATA_H
---
+++ Image.h
@@ -0,0 +1,82 @@
+#ifndef SOFA_OR_COMMON_FRAME_H
+#define SOFA_OR_COMMON_FRAME_H
+
+#include <SofaORCommon/Data.h>
+#include <opencv2/core.hpp>
+
+namespace sofa
+{
+namespace OR
+{
+namespace common
+{
+
+enum VideoMode
+{
+  MONO = 0,
+  STEREO_INTERLEAVED = 1,
+  STEREO_TOP_BOTTOM = 2,
+  STEREO_SIDE_BY_SIDE = 3,
+  STEREO_ROTATED_SIDE_BY_SIDE = 4
+};
+
+class Image : public cv::Mat, public sofa::OR::common::Data
+{
+ public:
+  Image() : cv::Mat() {}
+  Image(int rows, int cols, int type) : cv::Mat(rows, cols, type) {}
+  Image(cv::Size size, int type) : cv::Mat(size, type) {}
+  Image(int rows, int cols, int type, const cv::Scalar &s)
+      : cv::Mat(rows, cols, type, s)
+  {
+  }
+  Image(cv::Size size, int type, const cv::Scalar &s) : cv::Mat(size, type, s)
+  {
+  }
+  Image(int ndims, const int *sizes, int type) : cv::Mat(ndims, sizes, type) {}
+  Image(int ndims, const int *sizes, int type, const cv::Scalar &s)
+      : cv::Mat(ndims, sizes, type, s)
+  {
+  }
+  Image(const cv::Mat &m) : cv::Mat(m) {}
+  Image(int rows, int cols, int type, void *data, size_t step = AUTO_STEP)
+      : cv::Mat(rows, cols, type, data, step)
+  {
+  }
+
+  Image(cv::Size size, int type, void *data, size_t step = AUTO_STEP)
+      : cv::Mat(size, type, data, step)
+  {
+  }
+  Image(int ndims, const int *sizes, int type, void *data,
+        const size_t *steps = 0)
+      : cv::Mat(ndims, sizes, type, data, steps)
+  {
+  }
+
+  Image(const cv::Mat &m, const cv::Range &rowRange,
+        const cv::Range &colRange = cv::Range::all())
+      : cv::Mat(m, rowRange, colRange)
+  {
+  }
+
+  Image(const cv::Mat &m, const cv::Rect &roi) : cv::Mat(m, roi) {}
+  Image(const cv::Mat &m, const cv::Range *ranges) : cv::Mat(m, ranges) {}
+  inline friend std::istream &operator>>(std::istream &in,
+                                         Image __attribute__((__unused__)) & s)
+  {
+    return in;
+  }
+
+  inline friend std::ostream &operator<<(std::ostream &out, const Image &s)
+  {
+    out << s.cols << s.rows << s.type();
+    return out;
+  }
+};
+
+}  // namespace common
+}  // namespace OR
+}  // namespace sofa
+
+#endif  // SOFA_OR_COMMON_FRAME_H
