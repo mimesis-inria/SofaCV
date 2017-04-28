ba8738eba187c572a0cdd2c7c4680230453e039f
Bruno Marques <bruno.josue.marques@inria.fr>
2017-04-28 16:45:44 +0200

    All Projective transform related stuff is now using the new CameraSettings arch except for the FeatureTriangulator TODO: fix issue with OpenCV rotations in CameraSettings (composeP ?)

--- cvMatUtils.h
+++ cvMatUtils.h
@@ -59,6 +59,12 @@ void cvMat2sofaMat(const cv::Mat_<T>& src, defaulttype::Mat<L, C, T>& dst)
   assert(src.cols == C && src.rows == L);
   std::memcpy(dst.ptr(), src.data, src.total() * src.elemSize());
 }
+template <int L, int C, class T = float>
+void cvMat2sofaMat(const cv::Mat& src, defaulttype::Mat<L, C, T>& dst)
+{
+	assert(src.cols == C && src.rows == L);
+	std::memcpy(dst.ptr(), src.data, src.total() * src.elemSize());
+}
 
 template <int L, int C, class T = float>
 void sofaMat2cvMat(const defaulttype::Mat<L, C, T>& src, cv::Mat_<T>& dst)
@@ -66,12 +72,24 @@ void sofaMat2cvMat(const defaulttype::Mat<L, C, T>& src, cv::Mat_<T>& dst)
   dst = cv::Mat_<T>(L, C);
   std::memcpy(dst.ptr(), src.ptr(), L * C * sizeof(T));
 }
+template <int L, int C, class T = float>
+void sofaMat2cvMat(const defaulttype::Mat<L, C, T>& src, cv::Mat& dst)
+{
+	dst = cv::Mat_<T>(L, C);
+	std::memcpy(dst.ptr(), src.ptr(), L * C * sizeof(T));
+}
 
 template <int N, class T>
 void cvMat2sofaVector(const cv::Mat_<T>& src, defaulttype::Vec<N, T>& dst)
 {
   assert(src.total() == N);
   std::memcpy(dst.ptr(), src.data, src.total() * src.elemSize());
+}
+template <int N, class T>
+void cvMat2sofaVector(const cv::Mat& src, defaulttype::Vec<N, T>& dst)
+{
+	assert(src.total() == N);
+	std::memcpy(dst.ptr(), src.data, src.total() * src.elemSize());
 }
 
 template <class T>
@@ -84,12 +102,29 @@ void cvMat2sofaVector(const cv::Mat_<T>& src, helper::vector<T>& dst)
     dst.push_back(ptr[i]);
   }
 }
+template <class T>
+void cvMat2sofaVector(const cv::Mat& src, helper::vector<T>& dst)
+{
+	dst.reserve(src.total());
+	const T* ptr = reinterpret_cast<T*>(src.data);
+	for (size_t i = 0; i < src.total(); ++i)
+	{
+		dst.push_back(ptr[i]);
+	}
+}
 
 template <class T>
 void sofaVector2cvMat(const helper::vector<T>& src, cv::Mat_<T>& dst)
 {
-  dst = cv::Mat_<T>(src.size(), 1);
-  std::memcpy(dst.ptr(), src.data(), src.size() * sizeof(T));
+	dst = cv::Mat_<T>(src.size(), 1);
+	std::memcpy(dst.ptr(), src.data(), src.size() * sizeof(T));
+}
+
+template <class T>
+void sofaVector2cvMat(const helper::vector<T>& src, cv::Mat& dst)
+{
+	dst = cv::Mat_<T>(src.size(), 1);
+	std::memcpy(dst.ptr(), src.data(), src.size() * sizeof(T));
 }
 
 template <int N, class T>
@@ -97,6 +132,12 @@ void sofaVector2cvMat(const defaulttype::Vec<N, T>& src, cv::Mat_<T>& dst)
 {
   dst = cv::Mat_<T>(src.size(), N);
   std::memcpy(dst.ptr(), src.data(), src.size() * N * sizeof(T));
+}
+template <int N, class T>
+void sofaVector2cvMat(const defaulttype::Vec<N, T>& src, cv::Mat& dst)
+{
+	dst = cv::Mat_<T>(src.size(), N);
+	std::memcpy(dst.ptr(), src.data(), src.size() * N * sizeof(T));
 }
 
 }  // namespace matrix
