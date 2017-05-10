1cb898f1e0660870ce664a395ad0a60a7a8bc692
Bruno Marques <bruno.josue.marques@inria.fr>
2017-05-10 15:46:22 +0200

    FIX: - Compile issues repository tree restructuration - Simplification of cmake generation steps by auto-enabling / disabling features - OpenCV_contrib features are disabled by default, enabled when SOFAOR_OPENCV_CONTRIB_ENABLED=ON

--- cvMatUtils.h
+++ cvMatUtils.h
@@ -3,7 +3,6 @@
 
 #include <sofa/defaulttype/Mat.h>
 
-#include <igtlImageMessage.h>
 #include <opencv2/opencv.hpp>
 
 namespace sofa
@@ -22,32 +21,6 @@ enum VideoMode
   STEREO_SIDE_BY_SIDE = 3,
   STEREO_ROTATED_SIDE_BY_SIDE = 4
 };
-
-//#ifdef SOFAOR_ENABLE_OPENIGTLINK
-
-inline int getOpenCVDepth(int depth)
-{
-  switch (depth)
-  {
-    case CV_8S:
-      return igtl::ImageMessage::TYPE_INT8;
-    case CV_8U:
-      return igtl::ImageMessage::TYPE_UINT8;
-    case CV_16S:
-      return igtl::ImageMessage::TYPE_INT16;
-    case CV_16U:
-      return igtl::ImageMessage::TYPE_UINT16;
-    case CV_32S:
-      return igtl::ImageMessage::TYPE_INT32;
-    case CV_32F:
-      return igtl::ImageMessage::TYPE_FLOAT32;
-    case CV_64F:
-      return igtl::ImageMessage::TYPE_FLOAT64;
-    default:
-      return 0;
-  }
-}
-//#endif  // SOFAOR_ENABLE_OPENIGTLINK
 
 }  // namespace image
 
