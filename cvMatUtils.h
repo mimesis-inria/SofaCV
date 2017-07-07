f484a09e1b71750956bcf9fbff2429b84b6fdee3
Bruno Marques <bruno.josue.marques@inria.fr>
2017-07-07 16:45:55 +0200

    ADD: Authors.txt + Headers in each source code file

--- cvMatUtils.h
+++ cvMatUtils.h
@@ -1,3 +1,25 @@
+/******************************************************************************
+*       SOFAOR, SOFA plugin for the Operating Room, development version       *
+*                        (c) 2017 INRIA, MIMESIS Team                         *
+*                                                                             *
+* This program is a free software; you can redistribute it and/or modify it   *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 1.0 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+* Authors: Bruno Marques and external contributors (see Authors.txt)          *
+*                                                                             *
+* Contact information: contact-mimesis@inria.fr                               *
+******************************************************************************/
+
 #ifndef SOFA_OR_COMMON_CVMATUTILS_H
 #define SOFA_OR_COMMON_CVMATUTILS_H
 
